cadb56c05eba8ddebc2739b7d3c5ba53d5211334
Rahul-Pawar4912 <109332512+Rahul-Pawar4912@users.noreply.github.com>
2023-02-05 15:50:42 +0530

    second commit

--- BookStore/BookStore/Program.cs
+++ BookStore/BookStore/Program.cs
@@ -1,6 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapDefaultControllerRoute();
+if (app.Environment.IsDevelopment())
+{
+    app.MapGet("/Rahul", () => app.Environment.EnvironmentName);
+}
+//app.MapGet("/", () =>"Hello Rahul");
+
 
 app.Run();
